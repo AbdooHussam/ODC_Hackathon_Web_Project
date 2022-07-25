87cb92e091628d0c46da11379194fb0e82d9de61
AbdooHussam <Booodyhussamh@gmail.com>
2022-07-25 19:46:25 +0200

    home 3

--- lib/core/widgets/custom_text_button.dart
+++ lib/core/widgets/custom_text_button.dart
@@ -1,25 +1,29 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+
 class CustomTextButton extends StatelessWidget {
-   String ?text;
-   Function? function;
-    CustomTextButton({Key? key,
-    required this.text,
-    required this.function
-  }) : super(key: key) ;
+  String? text;
+  Function? function;
+  TextStyle? textStyle;
+
+  CustomTextButton(
+      {Key? key,
+      required this.text,
+      required this.function,
+      this.textStyle = const TextStyle(color: Colors.white, fontSize: 15)})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
       child: TextButton(
-        onPressed: (){
+        onPressed: () {
           function!();
         },
-        child:  AutoSizeText(
+        child: AutoSizeText(
           text!,
           maxLines: 1,
-          style:
-          TextStyle(color: Colors.white, fontSize: 15),
+          style: textStyle,
         ),
       ),
     );
